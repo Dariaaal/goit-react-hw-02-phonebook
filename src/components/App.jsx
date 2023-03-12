10a5a11b32546cbe0c0b9d7505f509d8528b0f96
Dariaaal <1406dariaal@gmail.com>
2023-03-13 01:27:29 +0200

    cardform

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+import CardForm from "./CardForm";
+
+export class App extends Component {
+
+  state = {
+    contacts: [],
+  };
+
+  formSubmitHandler = data => {
+    console.log(data);
+  }
+
+  render(){
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+      <CardForm onSubmit={this.formSubmitHandler}/>
+      </div>
+    );
+  }
 };
